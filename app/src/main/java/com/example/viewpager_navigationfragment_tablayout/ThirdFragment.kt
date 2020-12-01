7e1fac0199ca2f1fbcf6c3ca96803762c2317ca6
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-12-02 00:34:14 +0400

    esfsf

--- app/src/main/java/com/example/viewpager_navigationfragment_tablayout/ThirdFragment.kt
+++ app/src/main/java/com/example/viewpager_navigationfragment_tablayout/ThirdFragment.kt
@@ -1,7 +1,15 @@
 package com.example.viewpager_navigationfragment_tablayout
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 
 class ThirdFragment : Fragment(R.layout.fragment_third) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+    }
 }
--- app/src/main/java/com/example/viewpager_navigationfragment_tablayout/view_pager_pages/ThirdPageFragment.kt
+++ app/src/main/java/com/example/viewpager_navigationfragment_tablayout/view_pager_pages/ThirdPageFragment.kt
@@ -1,4 +1,4 @@
-package com.example.viewpager_navigationfragment_tablayout.pages_view_pager
+package com.example.viewpager_navigationfragment_tablayout.view_pager_pages
 
 import androidx.fragment.app.Fragment
 import com.example.viewpager_navigationfragment_tablayout.R
